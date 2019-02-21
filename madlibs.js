5d63636594ff543957c331fb13f5ed4bbd803098
Foxbyte <43650794+Foxbyte14@users.noreply.github.com>
2019-02-21 16:14:30 -0600

    Update madlibs.js

--- madlibs.js
+++ madlibs.js
@@ -3,15 +3,19 @@
 <!--  
  
      This is a mad libs game I made for a project
-     on @KhanAcademy 
+     on @KhanAcademy.org
 
      made with JavaScript
 
-     I will be adding it to my website soon.
+     I will be adding it to my website soon. https://foxbyte14.github.io/
 
      If you want to play it now go to:
 
      https://www.khanacademy.org/computer-programming/mad-libs/5821356050382848
+
+     all my projects from KA 
+     
+     https://www.khanacademy.org/profile/Foxbyte14/projects
     
  -->
     <head>
